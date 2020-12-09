f049c73229d409d0135e65d9ac2939588a463678
Paul <paulzuliani@gmail.com>
2020-12-09 12:34:09 +0100

    final gatsby site

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,65 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: 'Mon Full Stack Botcamp',
+    author: 'Moi-Meme',
+  },
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-plugin-react-helmet`,
+          {
+            resolve: `gatsby-source-contentful`,
+            options: {
+              // spaceId: `your_space_id`,
+              spaceId: process.env.CONTENTFUL_SPACE_ID,
+              // Learn about environment variables: https://gatsby.dev/env-vars
+              accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+            },
+          },
+          // `gatsby-remark-relative-images`,
+          // {
+          //   resolve: `gastby-remark-images`,
+          //   options: {
+          //     maxWidth: 750,
+          //     linkImagesToOriginal: false
+          //   }
+          // }
+          // gatsby-remark-relative-images must go before gatsby-remark-images
+          {
+            resolve: `gatsby-remark-relative-images`,
+            options: {
+              // [Optional] The root of "media_folder" in your config.yml
+              // Defaults to "static"
+              // staticFolderName: 'static',
+              // [Optional] Include the following fields, use dot notation for nested fields
+              // All fields are included by default
+              include: ['featured'],
+              // [Optional] Exclude the following fields, use dot notation for nested fields
+              // No fields are excluded by default
+              exclude: ['featured.skip'],
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: { 
+              maxWidth: 750,
+              linkImagesToOriginal: false },
+          },
+        ]
+      }
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,25 @@
 import React from "react"
+import { Link } from 'gatsby'
 
-export default function Home() {
-  return <div>Hello world!</div>
+// import Header from '../components/header'
+// import Footer from '../components/footer'
+
+import Layout from '../components/layout'
+import Head from '../components/head'
+
+// export default function Home() {
+//   return <div>Hello world!</div>
+// }
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <Head title="home" />
+      <h1>Hello,</h1>
+      <h2>I'm Andrew, a full stack dev...</h2>
+      <p>Need a dev ? <Link to="/contact">Contact me</Link></p>
+    </Layout>
+  )
 }
+
+export default IndexPage
